9c48aa2320da78348ca6a782a8d2a17e003f3bfb
ms <madinabonus258@gmail.com>
2026-07-24 20:19:08 +0500

    Add event listeners for navigation buttons

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 console.log("Evergreen started 🌲");
+
+
+// ===========================
+// Evergreen App
+// ===========================
+
+// Screens
+const welcomeScreen = document.getElementById("welcome");
+const interestsScreen = document.getElementById("interests");
+const homeScreen = document.getElementById("home");
+
+// Buttons
+const nextBtn = document.getElementById("nextBtn");
+const continueBtn = document.getElementById("continueBtn");
+
+// ===========================
+// Open Interests
+// ===========================
+
+nextBtn.addEventListener("click", () => {
+
+    welcomeScreen.classList.remove("active");
+    interestsScreen.classList.add("active");
+
+});
+
+// ===========================
+// Open Home
+// ===========================
+
+continueBtn.addEventListener("click", () => {
+
+    interestsScreen.classList.remove("active");
+    homeScreen.classList.add("active");
+
+});
