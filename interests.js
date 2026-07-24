95d0077d722a0bca395b36338dad5c1cc651dcce
ms <madinabonus258@gmail.com>
2026-07-24 20:46:26 +0500

    Create interests.js

---
+++ interests.js
@@ -0,0 +1,59 @@
+const interests = {
+
+"📚 Learning":[
+"📖 Reading",
+"📚 Books",
+"🇬🇧 English",
+"📝 IELTS",
+"🎯 SAT",
+"✍️ Writing",
+"🗣 Speaking"
+],
+
+
+"🔬 Science":[
+"🚀 Space",
+"🌿 Nature",
+"🧬 Biology",
+"⚛ Physics",
+"🌍 Earth",
+"🌌 Astronomy"
+],
+
+
+"💻 Technology":[
+"💻 Programming",
+"🌐 Web Development",
+"🤖 AI",
+"📱 App Development",
+"🎨 UI Design"
+],
+
+
+"🎨 Creativity":[
+"🎨 Drawing",
+"📷 Photography",
+"🎵 Music",
+"💃 Dancing",
+"🍳 Cooking",
+"🧁 Baking"
+],
+
+
+"🏃 Health":[
+"🚴 Cycling",
+"🏃 Running",
+"🧘 Yoga",
+"😴 Sleep",
+"🥗 Healthy Eating"
+],
+
+
+"🌱 Growth":[
+"📅 Discipline",
+"🎯 Goals",
+"💪 Confidence",
+"⏰ Time Management"
+]
+
+};
