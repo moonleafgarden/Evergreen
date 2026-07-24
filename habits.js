0586fa8b0c5a401250f3a95dc1465c680f5385d1
ms <madinabonus258@gmail.com>
2026-07-24 20:56:00 +0500

    Create habits.js

---
+++ habits.js
@@ -0,0 +1,24 @@
+const habitIdeas = {
+
+    "📖 Reading": "Read for 20 minutes",
+    "📚 Books": "Read a few pages",
+
+    "🇬🇧 English": "Practice English",
+    "📝 IELTS": "Study IELTS",
+
+    "🚀 Space": "Learn something about space",
+    "🌿 Nature": "Spend time outside",
+
+    "💻 Programming": "Code something",
+    "🌐 Web Development": "Build a website",
+
+    "🎨 Drawing": "Create something",
+    "💃 Dancing": "Practice dancing",
+
+    "🚴 Cycling": "Go cycling",
+    "🏃 Running": "Run today",
+
+    "📅 Discipline": "Complete your routine",
+    "🎯 Goals": "Work on your goal"
+
+};
